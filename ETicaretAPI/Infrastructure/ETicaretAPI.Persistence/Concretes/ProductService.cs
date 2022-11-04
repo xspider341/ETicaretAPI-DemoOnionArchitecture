3350e6b38f205483e675562855ddc35650641664
Mehmet Emin SOYSAL <mehmeteminsoysal@kik.gov.tr>
2022-11-04 17:34:24 +0300

    ok

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+            => new()
+            {
+                new() { Id= Guid.NewGuid(), Name ="Product 1", Price = 100, Stock = 10 },
+                new() { Id= Guid.NewGuid(), Name ="Product 2", Price = 100, Stock = 10 },
+                new() { Id= Guid.NewGuid(), Name ="Product 3", Price = 100, Stock = 10 },
+                new() { Id= Guid.NewGuid(), Name ="Product 4", Price = 100, Stock = 10 },
+                new() { Id= Guid.NewGuid(), Name ="Product 5", Price = 100, Stock = 10 },
+
+
+            };
+    }
+}
---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistence.Concretes;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=49153;Database=ETicaretAPIDb;"));
+        }
+    }
+}
